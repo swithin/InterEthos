bdc2615c92aa4fa22c354804ff2bc3bafc1ba845
swithin <josh@swithin.com>
2010-04-22 11:46:41 -0400

    Join Table YML file not migrating

--- db/migrate/20100310034537_data_add_collections_ontologies.rb
+++
@@ -1,9 +0,0 @@
-class DataAddCollectionsOntologies < ActiveRecord::Migration
-  def self.up
-    Collections_Ontology.load_from_file
-  end
-
-  def self.down
-    Collections_Ontology.delete_all
-  end
-end
