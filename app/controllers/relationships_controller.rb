70ce1491983f59b91f79542526e676cef51c3402
swithin <josh@swithin.com>
2010-04-23 09:17:22 -0400

    @Category added to Relationship views

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,17 +6,20 @@ class RelationshipsController < ApplicationController
   
   def show
     @relationship = Relationship.find(params[:id])
+    @category = @relationship.category
   end
   
   def new
     @relationship = Relationship.new
+    @category = Category.find(params[:category_id])
   end
   
   def create
     @relationship = Relationship.new(params[:relationship])
+    @category = @relationship.category
     if @relationship.save
       flash[:notice] = "Successfully created relationship."
-      redirect_to @relationship
+      redirect_to edit_category_path(:id => @category, :uroot_id => session[:uroot_id]) 
     else
       render :action => 'new'
     end
@@ -28,10 +31,10 @@ class RelationshipsController < ApplicationController
   
   def update
     @relationship = Relationship.find(params[:id])
-    @category = @relationship.category_id
+    @category = @relationship.category
     if @relationship.update_attributes(params[:relationship])
       flash[:notice] = "Successfully updated relationship."
-      redirect_to edit_category_path(:id => @category) 
+      redirect_to edit_category_path(:id => @category, :uroot_id => session[:uroot_id]) 
     else
       render :action => 'edit'
     end
@@ -39,8 +42,9 @@ class RelationshipsController < ApplicationController
   
   def destroy
     @relationship = Relationship.find(params[:id])
+    @category = @relationship.category
     @relationship.destroy
     flash[:notice] = "Successfully destroyed relationship."
-    redirect_to(:controller => 'categories', :action => 'show', :id => @category) 
+      redirect_to edit_category_path(:id => @category, :uroot_id => session[:uroot_id]) 
   end
 end
