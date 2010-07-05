bff5d447557fc83890a8cddf5c359f88062ee8bb
Josh Shortlidge <josh@swithin.com>
2010-07-05 17:32:47 -0400

    Enable "declarative_authorization" for "Teams"

--- db/migrate/20100101000010_create_teams.rb
+++ db/migrate/20100101000010_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration
     create_table :teams do |t|
       t.string :name
       t.text :description
-      t.integer :privacy
+      t.string :privacy
       t.references :user
 
       t.timestamps
