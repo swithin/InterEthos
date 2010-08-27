ef7bd25e652ce0709d221b852e6f21d87f3a8bd0
Josh Shortlidge <josh@swithin.com>
2010-08-27 10:34:50 -0400

    New Blogs and Topics - Version 1.0!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :roles
+  map.resources :posts
 
+  map.resources :topics
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
