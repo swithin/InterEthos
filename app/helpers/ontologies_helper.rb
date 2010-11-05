9fb9ad90df2ccc1b9dcddc2de3ecc05fa7440b37
Josh Shortlidge <josh@swithin.com>
2010-11-05 18:33:38 -0400

    Remove old "Ontology" named files and folders.

--- app/helpers/ontologies_helper.rb
+++
@@ -1,8 +0,0 @@
-module OntologiesHelper
-  def render_tree(roots, depth=0, &block)
-    roots.children.sort_by {|f| f.name}.each do | child |
-      yield(child, depth)
-      render_tree(child, depth+1, &block)
-    end
-  end
-end
--- app/models/ontology_user.rb
+++
@@ -1,8 +0,0 @@
-class OntologyUser < ActiveRecord::Base
-  using_access_control
-  
-  belongs_to :ontology
-  belongs_to :user
-  validates_presence_of :user, :ontology
-  validates_associated :user, :ontology
-end
